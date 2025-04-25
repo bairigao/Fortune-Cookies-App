94a08d153206df9737f21211ea8dbe4dcd1f2e1c
Luke Kempson <n11795816@qut.edu.au>
2025-04-26 00:21:49 +1000

    Added DB Files, cleaned up unused files from sample template

--- src/main/java/com/example/fortune_cookies_app/LoginController.java
+++ src/main/java/com/example/fortune_cookies_app/LoginController.java
@@ -5,10 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,7 +14,7 @@ import java.io.IOException;
 public class LoginController {
     public TextField loginEmail;
     public PasswordField loginPassword;
-    private UserDAO userDAO;
+    private final UserDAO userDAO;
     public TextField firstName;
     public TextField lastName;
     public TextField newEmail;
