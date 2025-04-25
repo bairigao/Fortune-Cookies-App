a5d0c7a93cd4b590a9c93ded9d37114432c9985a
catherine <catherine@hotmail.com>
2025-04-25 14:39:18 +1000

    Created Signup GUI

---
+++ src/main/java/com/example/fortune_cookies_app/Login.java
@@ -0,0 +1,36 @@
+package com.example.fortune_cookies_app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login extends Application {
+    public static final String TITLE = "Fortune Cookie App";
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    private static Stage setStage;
+
+    @Override
+    public void start(Stage loginStage) throws IOException {
+        setStage = loginStage;
+        Parent root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        loginStage.setTitle(TITLE);
+        loginStage.setScene(scene);
+        loginStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent base = FXMLLoader.load(getClass().getResource(fxml));
+        setStage.getScene().setRoot(base);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+    }
+
---
+++ src/main/java/com/example/fortune_cookies_app/LoginController.java
@@ -0,0 +1,59 @@
+package com.example.fortune_cookies_app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    @FXML
+    private TextField loginUsername;
+    @FXML
+    private PasswordField loginPassword;
+    @FXML
+    private TextField newUsername;
+    @FXML
+    private PasswordField newPassword;
+    @FXML
+    private PasswordField confirmPassword;
+
+    //change where the buttons will lead the user accordingly later,
+    //currently only leads from login to signup and vice versa
+
+    //when Sign Up button is clicked on Login screen
+    public void onSignupClick() throws IOException {
+        toSignUp();
+    }
+
+    //when Login button is clicked on Login screen
+    public void onLoginClick() throws IOException {
+        toSignUp();
+    }
+
+    //when Back button is clicked on Signup screen
+    public void onBackClick() throws IOException {
+        toLogin();
+    }
+
+    //when Sign Up button is clicked on Signup screen
+    public void onSignupConfirm() throws IOException {
+        toLogin();
+    }
+
+    //changes scene to login screen
+    protected void toLogin() throws IOException {
+        Login loginScreen = new Login();
+        loginScreen.changeScene("login-view.fxml");
+    }
+
+    //changes scene to sign up
+    protected void toSignUp() throws IOException {
+        Login loginScreen = new Login();
+        loginScreen.changeScene("signup-view.fxml");
+
+    }
+
+}
