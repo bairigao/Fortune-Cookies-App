4857e63419b426a1c05f7d392735073a72fcd4e3
Chris <n11811749@qut.edu.au>
2025-05-10 13:51:51 +1000

    handle response structure and JSON parsing

---
+++ src/main/java/com/example/fortune_cookies_app/model/OllamaResponse.java
@@ -0,0 +1,22 @@
+package com.example.fortune_cookies_app.model;
+import com.google.gson.Gson;
+
+public class OllamaResponse {
+
+    public String model;
+    public String created_at;
+    public String response;
+    public String done;
+
+
+    public String getResponse() {
+        return response;
+    }
+
+    public static OllamaResponse fromJson(String body) {
+        //for documentation of how to decode JSON response https://github.com/ollama/ollama/blob/main/docs/api.md
+        Gson gson = new Gson();
+        OllamaResponse response = gson.fromJson(body, OllamaResponse.class);
+        return response;
+    }
+}
