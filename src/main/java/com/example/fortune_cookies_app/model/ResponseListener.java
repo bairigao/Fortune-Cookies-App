8801ce2ca65a812830e85a9aa32f609842157951
Chris <n11811749@qut.edu.au>
2025-05-14 06:25:32 +1000

    Added comments

--- src/main/java/com/example/fortune_cookies_app/model/ResponseListener.java
+++ src/main/java/com/example/fortune_cookies_app/model/ResponseListener.java
@@ -1,5 +1,9 @@
 package com.example.fortune_cookies_app.model;
 
+
+/**
+ * Interface to define the response listener
+ */
 public interface ResponseListener {
     public void onResponseReceived(OllamaResponse response);
 }
