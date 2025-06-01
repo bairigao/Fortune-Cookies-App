9ba20df9fb2844d345c7960cb8427ad3d88ef079
Luke Kempson <n11795816@qut.edu.au>
2025-06-01 13:09:03 +1000

    Added package javadocs

---
+++ src/main/java/com/example/fortune_cookies_app/controller/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the controllers for the application
+ */
+package com.example.fortune_cookies_app.controller;
---
+++ src/main/java/com/example/fortune_cookies_app/model/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the core logic for the application to function.
+ * <p>
+ *     This package includes classes for data persistence, AI integration, and user authentication validation
+ * </p>
+ */
+package com.example.fortune_cookies_app.model;
